5305369357b3ccccf4486c4f8375ffed920a3dcc
vivienneobrien <vivienneobrienis@gmail.com>
2022-11-04 19:18:30 +0000

    added blog data obejct to array instead of dummy data items and then wrote about what i learnt today

--- src/components/AllCategories/AllCategories.tsx
+++ src/components/AllCategories/AllCategories.tsx
@@ -16,29 +16,19 @@ import styles from "./AllCategories.module.css";
 //   return <ul className={styles.Categories}>{getCategories}</ul>;
 // };
 
-const items = [
-  { categories: ["React", "Javascript"] },
-  { categories: ["CSS", "CSHCKSB"] },
-];
-
 const AllCategories = () => {
   const categories = Array.from(
-    new Set(items.flatMap(({ categories }) => categories))
+    new Set(BlogDataObject.flatMap(({ categories }) => categories))
   );
   console.log(categories);
 
   return (
     <>
-      {categories.map(
-        (
-          e,
-          key // if you use {} you will return undefined, if you do use {} then you need to put return before the curlies // U can use curly braces, but in this case u need to add return before JSX.
-        ) => (
-          <li key={key} className={styles.Categories}>
-            {e}
-          </li>
-        )
-      )}
+      {categories.map((e, key) => (
+        <li key={key} className={styles.Categories}>
+          {e}
+        </li>
+      ))}
     </>
   );
 };
